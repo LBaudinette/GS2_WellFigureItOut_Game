af618d34dc842e349513ff5341b87c9d13f4f1f6
rmit-s3722631-Daniel-Snowsill <41409058+rmit-s3722631-Daniel-Snowsill@users.noreply.github.com>
2020-08-21 15:57:39 +1000

    Added basic movement
    movement including jumping and gravity added

---
+++ GS2 - Assignment 2/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public float rotationSpeed;
+    public Transform camera;
+    private float minVertical = -45.0f;
+    private float maxVertical = 45.0f;
+    private float rotationY = 0;
+    private float rotationX = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotationX -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        rotationX = Mathf.Clamp(rotationX, minVertical, maxVertical);
+
+        rotationY += Input.GetAxis("Mouse X") * rotationSpeed;
+
+
+        transform.eulerAngles = new Vector3(0, rotationY, 0);
+        camera.transform.eulerAngles = new Vector3(rotationX, rotationY, 0);
+    }
+}
---
+++ GS2 - Assignment 2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed, slopeForce, jumpForce, groundDistance;
+    public LayerMask groundMask;
+    public Transform groundCheck;
+    private bool isGrounded;
+    private CharacterController charController;
+    private bool isWallRunning, isJumping;
+    private Animator animator;
+    private float gravity = -9.81f; //default value of gravity in Unity
+    Vector3 velocity; // Used for gravity
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+
+    }
+
+    //Jumping and gravity obtained from: https://youtu.be/_QajrabyTJc
+    private void movement() {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0) {
+            velocity.y = -2f;
+        }
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal") * speed, 0.0f, Input.GetAxisRaw("Vertical") * speed);
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        charController.Move(movement);
+
+        if(Input.GetButtonDown("Jump")) {
+            velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        if (onSlope()) {
+            charController.Move(Vector3.down * slopeForce * Time.deltaTime);
+        }
+
+        charController.Move(velocity * Time.deltaTime);
+
+        
+
+
+    }
+
+    //checks if the player is on a slope
+    private bool onSlope() {
+        RaycastHit hit;
+
+        //if the normal of the surface the player is standing on is not pointing up, then it is a slope
+        if(Physics.Raycast(transform.position, Vector3.down, out hit, 10)) {
+            if(hit.normal != Vector3.up) {
+                
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void Jump() {
+        print("JUMP");
+    }
+
+
+}
