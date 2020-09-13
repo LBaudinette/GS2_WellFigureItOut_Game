679b915081db6ea342bc5e977ba3e56bf8da27bf
Daniel <danielsnowsill@live.com.au>
2020-09-13 20:19:52 +1000

    Added main menu and level selector
    Added a main menu and a level selector. Quit game button also works

---
+++ GS2 - Assignment 2/Assets/Scripts/Menu Scripts/LevelSelectScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectScript : MonoBehaviour
+{
+    //Canvas for the main menu screen
+    public GameObject mainMenuPrefab;
+    //Canvas that the Button Select game object is a child of
+    public GameObject parentCanvas;
+    private string levelSelected;
+    public void buttonAction(GameObject buttonPressed) {
+        
+        switch (buttonPressed.name) {
+            case "Main Menu Button":
+                Instantiate(mainMenuPrefab);
+                Destroy(parentCanvas);
+                break;
+            case "Continue Button":
+                SimpleSceneFader.ChangeSceneWithFade(levelSelected);
+                break;
+            case "Level 1 Button":
+                levelSelected = "Level 1";
+                break;
+            case "Level 2 Button":
+                levelSelected = "Level 2";
+                break;
+
+        }
+    }
+}
---
+++ GS2 - Assignment 2/Assets/Scripts/Menu Scripts/MainMenuScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    //Canvas for select level screen
+    public GameObject selectLevelPrefab;
+    //Canvas that the Button Select game object is a child of
+    public GameObject parentCanvas;
+
+    public void buttonAction(GameObject buttonPressed) {
+        switch (buttonPressed.name) {
+            case "Play Button":
+                SimpleSceneFader.ChangeSceneWithFade("Level 1");
+                break;
+            case "Level Select Button":
+                Instantiate(selectLevelPrefab);
+                Destroy(parentCanvas);
+                break;
+            case "Quit Game Button":
+                //If the game is running in Unity, different quit function is needed
+                #if UNITY_EDITOR
+                    UnityEditor.EditorApplication.isPlaying = false;
+                #else
+                    Application.Quit();
+                #endif
+                break;
+
+
+
+        }
+    }
+}
