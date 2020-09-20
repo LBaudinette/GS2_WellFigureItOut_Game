a7ba19e40c29f6bdfbdf244b3e6cff11fd296227
Daniel <danielsnowsill@live.com.au>
2020-09-20 18:30:10 +1000

    Added Pause canvas

---
+++ GS2 - Assignment 2/Assets/Scripts/UI Scripts/MainMenuScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    //Canvas for select level screen
+    public GameObject selectLevelPrefab;
+    //Canvas that the Button Select game object is a child of
+    private GameObject parentCanvas;
+
+    private void Start() {
+        parentCanvas = transform.root.gameObject;
+    }
+    public void buttonAction(GameObject buttonPressed) {
+        switch (buttonPressed.name) {
+            case "Play Button":
+                SimpleSceneFader.ChangeSceneWithFade("Level 1");
+                break;
+            case "Level Select Button":
+                Instantiate(selectLevelPrefab);
+                Destroy(parentCanvas);
+                break;
+            case "Quit Game Button":
+                //If the game is running in Unity, different quit function is needed
+                #if UNITY_EDITOR
+                    UnityEditor.EditorApplication.isPlaying = false;
+                #else
+                    Application.Quit();
+                #endif
+                break;
+
+
+
+        }
+    }
+}
--- GS2 - Assignment 2/Assets/Scripts/UI Scripts/PauseScript.cs
+++ GS2 - Assignment 2/Assets/Scripts/UI Scripts/PauseScript.cs
@@ -3,26 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenuScript : MonoBehaviour
+public class PauseScript : MonoBehaviour
 {
     //Canvas for select level screen
     public GameObject selectLevelPrefab;
-    //Canvas that the Button Select game object is a child of
-    public GameObject parentCanvas;
 
+    //Canvas for Main Menu screen
+    public GameObject mainMenuPrefab;
+
+    //Canvas that the Button Select game object is a child of
+    private GameObject parentCanvas;
+
+    private void Start() {
+        parentCanvas = transform.root.gameObject;
+    }
     public void buttonAction(GameObject buttonPressed) {
         switch (buttonPressed.name) {
-            case "Play Button":
-                SimpleSceneFader.ChangeSceneWithFade("Level 1");
+            case "Resume Button":
+                Destroy(parentCanvas);
+                break;
+            case "Retry Button":
+                //Reload scene 
+                SimpleSceneFader.ChangeSceneWithFade(SceneManager.GetActiveScene().name);
                 break;
             case "Level Select Button":
                 Instantiate(selectLevelPrefab);
                 Destroy(parentCanvas);
                 break;
+            case "Main Menu Button":
+                Instantiate(mainMenuPrefab);
+                Destroy(parentCanvas);
+                break;
             case "Quit Game Button":
                 //If the game is running in Unity, different quit function is needed
                 #if UNITY_EDITOR
-                    UnityEditor.EditorApplication.isPlaying = false;
+                UnityEditor.EditorApplication.isPlaying = false;
                 #else
                     Application.Quit();
                 #endif
